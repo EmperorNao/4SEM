bdfd644c9c4e20ffb13841a294d7b051c7bff1cb
EmperorNao <asgasgagagwet@gmail.com>
2021-01-27 18:49:25 +0300

    Ended first part for first lab

---
+++ OOP/FilesInC/Binary.c
@@ -0,0 +1,141 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <locale.h> 
+
+#pragma warning (disable:4996)
+
+typedef struct {
+	char name[30];
+	char number[15];
+	char adress[30];
+} inform;
+
+// Вставляет структуру a в файл с именем s, не нарушая упорядоченности, если ее там нет
+void add_new_record(char* filename, inform a);
+
+// Ввод структуры a
+void read_struct(inform* a);
+
+// Вставляет структуру a в файл f, начиная с байта под номером i 
+void insert(FILE* f, long int i, inform a);
+
+// Возвращает номер байт в f, начиная с которого надо вставить a,
+// если вставка не требуется возвращает -1// 
+long int search_record(FILE* f, inform a);
+
+// Вывод записанных в файл s структур
+void write_f(char* s);
+
+
+int main() {
+
+	setlocale(LC_ALL, "RUS");
+	char filename[20];
+	inform a;
+
+	read_struct(&a);
+
+	printf("Введите название файла\n");
+	gets(filename);
+
+	printf("Содержимое файла до преобразования\n");
+	write_f(filename);
+
+	add_new_record(filename, a);
+
+	printf("Содержимое файла после преобразования\n");
+	write_f(filename);
+
+}
+
+void read_struct(inform* a) {
+
+	printf("Введите фамилию\n");
+	gets((*a).name);
+	printf("Введите номер\n");
+	gets((*a).number);
+	printf("Введите адрес\n");
+	gets((*a).adress);
+
+}
+
+
+void add_new_record(char* s, inform a) {
+
+	FILE* f;
+	f = fopen(s, "r+b");
+	long int i = search_record(f, a);
+	if (i != -1) {
+	
+		insert(f, i, a);
+
+	}
+	else {
+
+		printf("Запись найдена");
+
+	}
+	fclose(f);
+
+}
+
+
+long int search_record(FILE* f, inform a) {
+
+	inform rec;
+	long int k, p, i;
+	int flag = 0;
+
+	while (fread(&rec, sizeof(inform), 1, f) > 0)
+	{
+		flag = ftell(f);
+		p = strcmp(a.name, rec.name);
+
+		if (p == 0) {
+
+			return -1;
+
+		}
+		else if (p < 0) {
+
+			return(flag - sizeof(inform));
+
+		}
+	}
+	return flag;
+
+}
+
+
+void insert(FILE* f, long int i, inform a) {
+
+	inform rec;
+	long int j, e;
+	fseek(f, 0, SEEK_END);
+
+	j = ftell(f);
+	chsize(fileno(f), j + sizeof(inform));
+	e = j + sizeof(inform);
+
+	fseek(f, i, SEEK_SET);
+	while (j < e) {
+
+		fread(&rec, sizeof(inform), 1, f);
+		fseek(f, -(int)(sizeof(inform)), SEEK_CUR);
+		fwrite(&a, sizeof(inform), 1, f);
+		a = rec;
+		j = ftell(f);
+
+	}
+}
+
+
+void write_f(char* s) {
+	inform a;
+	FILE* f;
+	f = fopen(s, "rb");
+	while (fread(&a, sizeof(inform), 1, f) > 0)
+		printf("%s %s %s\n", a.name, a.adress, a.number);
+	fclose(f);
+}
---
+++ OOP/WinForms/MyForm.h
@@ -0,0 +1,74 @@
+#pragma once
+
+namespace WinForms {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::ToolTip^ toolTip1;
+	protected:
+	private: System::ComponentModel::IContainer^ components;
+
+	protected:
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->toolTip1 = (gcnew System::Windows::Forms::ToolTip(this->components));
+			this->SuspendLayout();
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(367, 413);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void listBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
